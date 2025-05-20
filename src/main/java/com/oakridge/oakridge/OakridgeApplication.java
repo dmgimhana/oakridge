2e2b69c0633377a8c88d215725e11a0e8bb413db
dmgimhana <dmgimhana@gmail.com>
2025-05-20 11:39:19 +0100

    project setup

---
+++ src/main/java/com/oakridge/oakridge/OakridgeApplication.java
@@ -0,0 +1,13 @@
+package com.oakridge.oakridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OakridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oakridge/oakridge/OakridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oakridge.oakridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OakridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
