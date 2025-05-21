d88f651a3b5ead575d264c986a961127e2265dd2
dmgimhana <dmgimhana@gmail.com>
2025-05-21 17:01:13 +0100

    Added thymeleaf

--- src/main/java/com/oakridge/oakridge/controller/HomeController.java
+++ src/main/java/com/oakridge/oakridge/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.oakridge.oakridge.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
-    public String displayHomePage(){
+    @RequestMapping({"", "/","home"})
+    public String displayHomePage(Model model){
+        model.addAttribute("username", "Gimhana");
         return "home.html";
     }
 }
