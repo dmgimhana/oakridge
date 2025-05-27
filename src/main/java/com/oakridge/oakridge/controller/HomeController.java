b35ee60283682c7052056028a2c31a673549614a
dmgimhana <dmgimhana@gmail.com>
2025-05-27 09:45:40 +0100

    added supporting files

--- src/main/java/com/oakridge/oakridge/controller/HomeController.java
+++ src/main/java/com/oakridge/oakridge/controller/HomeController.java
@@ -1,14 +1,12 @@
 package com.oakridge.oakridge.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping({"", "/","home"})
-    public String displayHomePage(Model model){
-        model.addAttribute("username", "Jim");
+    public String displayHomePage(){
         return "home.html";
     }
 }
