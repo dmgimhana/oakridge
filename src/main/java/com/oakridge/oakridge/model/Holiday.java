f04f785fd776482f8dbf29d9a4ebd5670f831d81
dmgimhana <dmgimhana@gmail.com>
2025-05-31 11:51:30 +0100

    Added Lombok

--- src/main/java/com/oakridge/oakridge/model/Holiday.java
+++ src/main/java/com/oakridge/oakridge/model/Holiday.java
@@ -1,5 +1,8 @@
 package com.oakridge.oakridge.model;
 
+import lombok.Data;
+
+@Data
 public class Holiday {
 
     private final String day;
@@ -11,24 +14,5 @@ public class Holiday {
         FEDERAL,
     }
 
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    
 
 }
