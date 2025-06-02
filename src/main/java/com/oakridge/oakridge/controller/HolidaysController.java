760ae165687622ae10aec3a24c3f7684110f7682
dmgimhana <dmgimhana@gmail.com>
2025-06-02 05:19:15 +0100

    displaying holidays conditionally using Query Params with the help of @RequestParam annotation

--- src/main/java/com/oakridge/oakridge/controller/HolidaysController.java
+++ src/main/java/com/oakridge/oakridge/controller/HolidaysController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.oakridge.oakridge.model.Holiday;
 
@@ -15,7 +16,13 @@ import com.oakridge.oakridge.model.Holiday;
 public class HolidaysController {
     
     @GetMapping("/holidays")
-    public String displayHolidays(Model model) {
+    public String displayHolidays(
+    @RequestParam(required = false) boolean festival ,  
+    @RequestParam(required = false) boolean federal, 
+    Model model) {
+
+        model.addAttribute("festival", festival);
+        model.addAttribute("federal", federal);
         
         List<Holiday> holidays = Arrays.asList(
             new Holiday("January 1", "New Year's Day", Holiday.Type.FESTIVAL),
